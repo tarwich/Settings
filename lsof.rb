4b6bfe9b4ecae977e9bbf0f6bed13dbedc619323
Samuel Dillow <tarwich@gmail.com>
2012-11-17 00:10:48 -0600

    Finished network chart

--- lsof.rb
+++ lsof.rb
@@ -3,106 +3,109 @@
 DOT_FILE = File.expand_path("~/Desktop/lsof.dot")
 IMAGE_FILE = DOT_FILE + ".png"
 
-class Node
-	def initialize name="", options={}
-		@name = name
-		@children = {}
-		@options = options
+class Document
+	def initialize
+		@applications = []
+		processLSOF
 	end
 
-	#
-	# Properties
-	#
-	def name     ; return @name     ; end
-	def children ; return @children ; end
-	def options  ; return @options  ; end
+	def processLSOF 
+		application = nil
+		connection = nil
 
-	#
-	# Methods
-	#
-	def addChild node
-		return children[node.name] = node
-	end
+		# Get the LSOF data
+		`lsof -Pi -F`.scan(/^(?:c|n|P|TST).*/).each{ |line|
+			field, value = line.scan(/(T..|.)(.*)/).flatten
 
-	def print stream=STDOUT, depth=0
-		indent = "\t" * depth
-		properties = formatOptions @options
-		stream.puts %{#{indent}"#{@name}" #{properties};}
-
-		@children.each {|name,child|
-			stream.puts %{#{indent}\t"#{@name}" -> "#{name}";}
-			child.print stream, depth+1
+			case field
+				when "c"
+					application = Application.new(:name=>value)
+					@applications.push(application)
+				when "P"
+					connection = Connection.new(:protocol=>value)
+					application.connections.push(connection)
+				when "n"
+					connection.processLSOF(value)
+				when "TST"
+					connection.status = value
+			end
 		}
 	end
 
-	def formatOptions options=@options
-		result = "["
-		options.each {|name,value| result += %{#{name}="#{value}"} }
-		result += "]"
-		
-		return result
+	def print stream=STDOUT, depth=0
+		stream.puts "digraph G {"
+		stream.puts "	overlap=false;"
+		stream.puts "	"
+		stream.puts %{"My Computer" [shape=box,style=filled,color="#dddddd"];}
+
+		@applications.each { |application| 
+			stream.puts %{"My Computer" -> "#{application.name}";}
+			application.print(stream, depth+1) 
+		}
+
+		stream.puts "}"
 	end
 end
 
-# Get the LSOF data
-lines = `lsof -Pi -F`.scan(/^(?:c|n|P|TST).*/)
+class Application
+	def initialize options={}
+		@name = options[:name] || ""
+		@connections = []
+	end
 
-# Create a node map so we don't double up nodes
-nodes = {}
-
-document = Node.new()
-nodes[:document] = document
-application = nil
-
-lines.each { |line| 
-	field, value = line.scan(/(T..|.)(.*)/).flatten
+	def name        ; return @name        ; end
+	def connections ; return @connections ; end
 	
-	case field
-	when "c" 
-		node = document.addChild(Node.new(value))
-	when "P" # protocol
-		node.options[:protocol] = value
-	when "n"
-		address = value
-	when "TST"
-		field, value = value.split("=")
-	when "u"
-	else
-		puts "Unknown field '#{field}' in #{value}"
+	def print stream=STDOUT, depth=0
+		indent = "\t" * depth
+		stream.puts %{#{indent}"#{@name}" [shape="ellipse",style=filled,color="#ddddff"];}
+		printedConnections = {}
+		printedPorts = {}
+
+		connections.each { |connection| 
+			if(!printedConnections[connection.destination]) then
+				printedConnections[connection.destination] = true
+				stream.puts %{#{indent}\t"#{@name}.#{connection.destination}" [shape=rectangle,label="#{connection.destination}"];}
+				stream.puts %{#{indent}\t"#{@name}" -> "#{@name}.#{connection.destination}";}
+			end
+
+			if(!printedPorts[connection.destinationPort]) then
+				printedPorts[connection.destinationPort] = true
+				stream.puts %{#{indent}\t"#{@name}.#{connection.destination}.#{connection.destinationPort}" [shape=plaintext,label="#{connection.destinationPort}"];}
+				stream.puts %{#{indent}\t"#{@name}.#{connection.destination}" -> "#{@name}.#{connection.destination}.#{connection.destinationPort}" [label="#{connection.protocol}"];}
+			end
+		}
 	end
+end
 
-	if(false) then
-		application, pid, protocol, connection = line.scan(/(\w+)\s+(\d+).*?(TCP|UDP) (.*?)$/)[0]
-		# Get the name of the application
-		application = `lsof -n -p #{pid}`.split("\n")[3]
-		
-		node = document.children[application] || Node.new(application, :shape=>"ellipse")
-		document.addChild node
-		
-		parts = connection.scan(/((.*)\s*->\s*(.*)\s+\((.*)\)|(.*?)\((.*?)\)|(^.*))/)[0]
-		source      = parts[1] || parts[4] || parts[6]
-		destination = parts[2] 
-		mode        = parts[3] || parts[5]
-
-		if(destination) then
-			child = Node.new destination, :shape=>"rectangle"
-			node.addChild child
-			
-			mode_ = mode ? ":#{mode}" : ""
-			#file.puts %{\t\t"#{application}" -> "#{destination}" [ label = "#{protocol}#{mode_}" ];}
-		end
+class Connection
+	def initialize options={}
+		@protocol    = options[:protocol] || ""
+		@source      = ""
+		@destination = ""
+		@mode        = ""
+		@status      = ""
 	end
-}
+	
+	def destination     ; return @destination     ; end
+	def destinationPort ; return @destinationPort ; end
+	def protocol        ; return @protocol        ; end
+	def protocol= value ; @protocol = value       ; end
+	def status= value   ; @status = value         ; end
+	
+	def processLSOF lsofData
+		parts = lsofData.scan(/^(?:(.*?):(.*?)\s*->\s*(.*?):(.*?)|(.*?):(.*?))$/)[0]
+		@source          = parts[0]
+		@sourcePort      = parts[1]
+		@destination     = parts[2] || parts[4]
+		@destinationPort = parts[3] || parts[5]
+	end
+end
 
-file = File.open(DOT_FILE, "w")
-file.puts "digraph G {"
-file.puts "\toverlap=false;"
-file.puts ""
-document.children.each {|name,node| node.print(file, 1) }
-file.puts "}"
-
-file.close
+stream = File.open(DOT_FILE, "w")
+Document.new.print stream
+stream.close
 
 `twopi -Tpng -o "#{IMAGE_FILE}" "#{DOT_FILE}"`
-#`open "#{IMAGE_FILE}"`
+`open "#{IMAGE_FILE}"`
 
