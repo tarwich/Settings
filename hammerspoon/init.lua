aef8981e1647d9a2cd87576c2b14975dce40c64c
Samuel Dillow <tarwich@gmail.com>
2018-01-13 17:04:32 -0600

    Updated config to spoons

---
+++ hammerspoon/init.lua
@@ -0,0 +1,15 @@
+
+-- HCalendar
+hs.loadSpoon('HCalendar')
+
+-- WinWin
+hs.loadSpoon('WinWin')
+hs.hotkey.bind({'cmd', 'alt'}, 'left', function() spoon.WinWin:moveAndResize('left') end)
+hs.hotkey.bind({'cmd', 'alt'}, 'right', function() spoon.WinWin:moveAndResize('right') end)
+hs.hotkey.bind({'cmd', 'alt'}, 'up', function() spoon.WinWin:moveAndResize('top') end)
+hs.hotkey.bind({'cmd', 'alt'}, 'down', function() spoon.WinWin:moveAndResize('bottom') end)
+hs.hotkey.bind({'cmd', 'alt'}, 'return', function() spoon.WinWin:moveAndResize('center') end)
+hs.hotkey.bind({'cmd', 'alt'}, 'c', function() spoon.WinWin:moveAndResize('center') end)
+
+-- Lockscreen
+hs.hotkey.bind({'cmd', 'ctrl', 'shift'}, 'L', 'Lock Screen', function() hs.caffeinate.lockScreen() end)
